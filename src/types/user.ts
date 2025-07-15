9bcf3d1fe5ff268d5b4425cd08b838076d501423
Ahmad Alfaridzi <ahmadalfaridzi.e@gmail.com>
2025-07-15 10:42:37 +0700

    Menghubungkan dengan postgreSQL

---
+++ src/types/user.ts
@@ -0,0 +1,13 @@
+
+import type { UserRole } from '@/types/user';
+
+export const roleToTitleCase: Record<UserRole, string> = {
+    ADMIN: 'Admin',
+    OWNER: 'Owner',
+    DIREKTUR: 'Direktur',
+    MANAJER: 'Manajer',
+    KARYAWAN: 'Karyawan',
+    TEKNISI: 'Teknisi'
+  };
+
+export * from './user';
