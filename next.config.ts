8c537b386b70f2622b83ceeb4ab523d64b4f5d01
Ahmad Alfaridzi <Ahmadalfaridzi5@gmail.com>
2025-07-29 10:50:38 +0700

    next config

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,9 @@
 const nextConfig = {
   reactStrictMode: true,
   devIndicators: false,
-  experimental: {
-    missingSuspenseWithCSRBailout: false 
-  }
-  eslint: {
-    ignoreDuringBuilds: true, // Sementara nonaktifkan ESLint saat build
-  },
-  typescript: {
-    ignoreBuildErrors: true, // Sementara izinkan TypeScript errors
-  },
+  // experimental: {
+  //   missingSuspenseWithCSRBailout: false 
+  // }
 }
 
 module.exports = nextConfig
